f976f7a43cc1b234f20519b66cf24eaf03b719f5
llqintel <gqjbbs@hotmail.com>
2015-03-18 16:51:15 +0800

    Site updated: 2015-03-18 16:51:14

--- js/main.js
+++ js/main.js
@@ -26,8 +26,7 @@ var Main = (function(){
 		var tags = $(".tagcloud a");
 		tags.css({"font-size": "12px"});
 		for(var i=0,len=tags.length; i<len; i++){
-			//var num = parseInt(Math.random()*5+1);
-			var num = tags.eq(i).html().length % 5 +1;
+			var num = parseInt(Math.random()*5+1);
 			tags[i].className = "";
 			tags.eq(i).addClass("color"+num);
 		}
@@ -113,9 +112,6 @@ var Main = (function(){
 			bind();
 			fancyInit();
 			Tips.init();
-			new Mobile({
-				ctn: document.getElementsByClassName("slider-trigger")[0]
-			});
 		}
 	}
 })();
